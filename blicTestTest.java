7c3237d4dbe8acb7842412df050c8539bb761930
dusanac <dusan.materic@telrad.net>
2013-02-07 04:59:02 -0800

    Create blicTestTest.java
    Non essential file. used just for tests when the code was changed.

---
+++ blicTestTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stestgenerator;
+
+/**
+ *
+ * @author dux
+ */
+public class blicTestTest {
+    
+    public static void main(String[] args) {
+       //blicTest bt = new blicTest (8,3,"biologija");
+       //blicTestHtml bt = new blicTestHtml (8,3,"biologija");
+       //bt.super.writeOutput("biologija");
+       //bt.writeOutput("biologija");
+       multivalueTestHtml mt = new multivalueTestHtml (15,6,"ekologija");
+    }
+    
+}
